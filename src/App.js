6fda00b1740adc27b92658b4362903f00f3527ba
kalph <kalpupadhyay@gmail.com>
2020-05-04 21:40:35 +0530

    app complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Card from "./components/imageCard";
+import Search from "./components/ImageSearch";
 
-function App() {
+const App = (props) => {
+  const [images, setImages] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto my-10">
+      <Search searchText={(text) => setTerm(text)} />
+
+      {!isloading && images.length === 0 && (
+        <h1 className="text-4xl text-center mx-auto">NO images found</h1>
+      )}
+
+      {isloading ? (
+        <h1 className="text-6xl text-center mx-auto">Page loading</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => {
+            return (
+              <Card
+                key={image.id}
+                image={image}
+                views={image.views}
+                name={image}
+                download={image.downloads}
+                likes={image.likes}
+                user={image.user}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
